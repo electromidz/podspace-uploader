e26af2440225600ac15a5ffff902687245c86a19
electromidz <electromidz@gmail.com>
2023-05-11 18:35:16 +0330

    Need pnpm

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-type IisEven = (arg: number) => boolean;
-interface IPodSpace {
-    isEven: IisEven;
-}
-declare class PodSpace implements IPodSpace {
-    isEven(num: number): boolean;
-}
-
-export { PodSpace as default };
--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-type IisEven = (arg: number) => boolean;
-interface IPodSpace {
-    isEven: IisEven;
-}
-declare class PodSpace implements IPodSpace {
-    isEven(num: number): boolean;
-}
-export default PodSpace;
-//# sourceMappingURL=index.d.ts.map
